b2a14ed3e7a9dd1a50db0a923edabba00769426d
Raza Hassan <bilgramiraza@yahoo.com>
2021-07-28 10:58:58 +0530

    Added Grid Generation and Drawing Logic

--- script.js
+++ script.js
@@ -1,20 +1,54 @@
 const grid = document.querySelector('.board');
-const penColor=document.querySelector('#penColor');
+const primary=document.querySelector('#primary');
+const secondary=document.querySelector('#secondary');
 const reset=document.querySelector('label>button');
+const dimensions=document.querySelector('#dimensions');
+let primaryPen=false;
+let secondaryPen=false;
+let box= [];
 
-function gridMaker(rows,colums){
-    for(let i =0;i<rows;i++)
-        for(let j =0;j<colums;j++){
-            const box = document.createElement('div');
-            box.classList.add('box');
-            grid.appendChild(box);
-        }
+window.onload=createGrid();
+
+function createGrid(){
+    grid.style.gridTemplateColumns=`repeat(${dimensions.value}, 1fr)`;
+    grid.style.gridTemplateRows=`repeat(${dimensions.value}, 1fr)`;
+    console.log(primary.value);
+    for(let i=0;i<(parseInt(dimensions.value)**2);i++){
+        box[i]=document.createElement('div');
+        box[i].classList.add('box');
+        grid.appendChild(box[i]);
+    };
+
 }
-window.onload = gridMaker(16,16);
-
-
-reset.addEventListener('click',()=>{
-    const rows=document.querySelector('#rows');
-    const columns=document.querySelector('#columns');
-    gridMaker(rows.value,columns.value)
+box.forEach((cell)=>{
+    cell.addEventListener('click',(event)=>{
+        penToggle(event.which);
+        if(primaryPen)
+            event.target.style.backgroundColor=primary.value;
+    });
+    cell.addEventListener('contextmenu',(event)=>{
+        event.preventDefault();
+        penToggle(event.which);
+        if(secondaryPen)
+            event.target.style.backgroundColor=secondary.value;
+    });
+    cell.addEventListener('mouseover',(event)=>{
+        if(primaryPen)
+            event.target.style.backgroundColor=primary.value;
+        else if(secondaryPen)
+            event.target.style.backgroundColor=secondary.value;
+    })
 });
+
+function penToggle(press){
+    switch(press){
+        case 1: primaryPen=!(primaryPen);
+                if(primaryPen)
+                    secondaryPen=false;
+                break;
+        case 3: secondaryPen=!(secondaryPen);
+                if(secondaryPen)
+                    primaryPen=false;
+                break;
+    }
+}
