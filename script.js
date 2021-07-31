af5df3619e91b24cd63aa1eb8a8610c9e3cdf4bd
Raza Hassan <bilgramiraza@yahoo.com>
2021-07-31 12:16:09 +0530

    Added Gridlines togggle

--- script.js
+++ script.js
@@ -4,19 +4,24 @@ const secondary=document.querySelector('#secondary');
 const dimensions=document.querySelector('#dimensions');
 const reset=document.querySelector('#reset');
 const pen=document.querySelectorAll('input[name="pen"]');
-let primaryPen=false;
-let secondaryPen=false;
+const gridLines=document.querySelector('input[name="gridLines"');
+let isPrimaryPen=false;
+let isSecondaryPen=false;
 let box= [];
-let rainbow=false;
-let lighten=false;
-let darken=false;
+let isRainbow=false;
+let isLighten=false;
+let isDarken=false;
 createGrid();
+const cell=document.querySelectorAll('.box');
 dimensions.addEventListener('input',createGrid);
 
 reset.addEventListener('click',()=>{
     location.reload();
 });
 
+gridLines.addEventListener('click',()=>{
+    cell.forEach((item)=>item.classList.toggle('outline'));
+});
 
 pen.forEach((item)=>{
     item.addEventListener('change',(event)=>{
@@ -27,14 +32,14 @@ pen.forEach((item)=>{
 
 function penToggle(press){
     switch(press){
-        case 1:primaryPen=!(primaryPen);
-            if(primaryPen)
-                secondaryPen=false;
+        case 1:isPrimaryPen=!(isPrimaryPen);
+            if(isPrimaryPen)
+                isSecondaryPen=false;
             break;
 
-        case 3: secondaryPen=!(secondaryPen);
-            if(secondaryPen)
-                primaryPen=false;
+        case 3: isSecondaryPen=!(isSecondaryPen);
+            if(isSecondaryPen)
+                isPrimaryPen=false;
             break;
     }
 }
@@ -52,28 +57,28 @@ function createGrid(){
 
         box[i].addEventListener('click',(event)=>{
             penToggle(event.which);
-            if(primaryPen)
+            if(isPrimaryPen)
                 event.target.style.backgroundColor=primary.value;
         });
         box[i].addEventListener('contextmenu',(event)=>{
             event.preventDefault();
             penToggle(event.which);
-            if(secondaryPen)
+            if(isSecondaryPen)
                 event.target.style.backgroundColor=secondary.value;
         });
         box[i].addEventListener('mouseover',(event)=>{
-            if(rainbow)
+            if(isRainbow)
                 primary.value='#'+Math.floor(Math.random()*16777215).toString(16).padStart(6,'0'); //https://dev.to/akhil_001/generating-random-color-with-single-line-of-js-code-fhj
-            else if(lighten)
+            else if(isLighten)
                 primary.value=LightenDarkenColor(event.target.style.backgroundColor,25);
-            else if(darken){
+            else if(isDarken){
                 primary.value=LightenDarkenColor(event.target.style.backgroundColor,-25);
                 console.log("darken");
             }
 
-            if(primaryPen)
+            if(isPrimaryPen)
                 event.target.style.backgroundColor=primary.value;
-            else if(secondaryPen)
+            else if(isSecondaryPen)
                 event.target.style.backgroundColor=secondary.value;
             
         });
@@ -81,30 +86,32 @@ function createGrid(){
     }
 }
 
+
+
 function penEffect(effect){
     switch(effect){
         case "normal":primary.value="#000000";
             break;
         case "eraser":primary.value="#ffffff";
             break;
-        case "rainbow":rainbow=true;
-            lighten=false;
-            darken=false;
+        case "rainbow":isRainbow=true;
+            isLighten=false;
+            isDarken=false;
             break;
-        case "lighten":lighten=true;
-            rainbow=false;
-            darken=false;
+        case "lighten":isLighten=true;
+            isRainbow=false;
+            isDarken=false;
             break;
-        case "darken":darken=true;
-            lighten=false;
-            rainbow=false;
+        case "darken":isDarken=true;
+            isLighten=false;
+            isRainbow=false;
             break;
     }
     secondary.value="#ffffff";
     if(effect!=="rainbow" && effect!=="lighten" && effect!=="darken"){
-        rainbow=false;
-        lighten=false;
-        darken=false;
+        isRainbow=false;
+        isLighten=false;
+        isDarken=false;
     }
 }
 function LightenDarkenColor(rgb,mod){
